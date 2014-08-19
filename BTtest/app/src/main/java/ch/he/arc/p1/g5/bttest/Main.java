261de518edcdb422d307ca918173178aeac0b75e
Claret Romain <romain.claret@rocla.ch>
2014-08-19 15:26:14 +0200

    1st page

---
+++ BTtest/app/src/main/java/ch/he/arc/p1/g5/bttest/Main.java
@@ -0,0 +1,87 @@
+package ch.he.arc.p1.g5.bttest;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+
+
+public class Main extends Activity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Button bTest1 = (Button) findViewById(R.id.bTest1);
+        //boolean onOff = true;
+        //bTest1.setText("Set to On");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        /*bTest1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //bTest1.setText("Set to Off");
+
+/*                if (onOff) {
+                    bTest1.setText("Set to Off");
+                }else{
+                    bTest1.setText("Set to On");
+                }
+            }
+
+        }); */
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
---
+++ TestBluetooth/app/src/main/java/ch/he/arc/p1/g5/testbluetooth/Main.java
@@ -0,0 +1,56 @@
+package ch.he.arc.p1.g5.testbluetooth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends Activity {
+
+    Button bOnOff = (Button) findViewById(R.id.bOnOff);
+
+    boolean onOff = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bOnOff.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+/*
+                if (onOff) {
+                    bOnOff.setText("Set to Off");
+                }else{
+                    bOnOff.setText("Set to On");
+                }*/
+            }
+
+        });
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
