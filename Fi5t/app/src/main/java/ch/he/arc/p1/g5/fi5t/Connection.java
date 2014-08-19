8ef320ac256f6bd8c4dbb156cb77872254c33ee8
unknown <raphael.schaffo@INF13-SCHAFFOR.intra.eiaj.ch>
2014-08-19 14:51:55 +0200

    no message

--- Fi5t/app/src/main/java/ch/he/arc/p1/g5/fi5t/Connection.java
+++ Fi5t/app/src/main/java/ch/he/arc/p1/g5/fi5t/Connection.java
@@ -1,17 +1,32 @@
 package ch.he.arc.p1.g5.fi5t;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Connection extends Activity {
-
+    Button bConnecter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        bConnecter=(Button)findViewById(R.id.bConnecter);
+        bConnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iConnectMain =new Intent(Connection.this,Main.class);
+                startActivity(iConnectMain);
+
+            }
+        });
+
+
     }
 
 
