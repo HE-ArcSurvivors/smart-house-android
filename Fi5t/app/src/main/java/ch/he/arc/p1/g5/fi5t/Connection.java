6ed528fb74140a8d3bfaf1c88ca5fd16a1bbde95
Claret Romain <romain.claret@rocla.ch>
2014-09-02 09:12:01 +0200

    bug

--- Fi5t/app/src/main/java/ch/he/arc/p1/g5/fi5t/Connection.java
+++ Fi5t/app/src/main/java/ch/he/arc/p1/g5/fi5t/Connection.java
@@ -235,7 +235,7 @@ public class Connection extends Activity {
 
                                                     startActivity(myIntent);
 
-                                                    finish();
+                                                    //finish();
 
 
 
